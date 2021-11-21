e6d373ba074b5e02d2f534597c934ee4e32b26f3
Mochamad Dena Eka Putra <sica4822@gmail.com>
2021-11-22 03:40:00 +0700

    add seeders

--- seeders/20211115114209-add-data-categories.js
+++ seeders/20211115114209-add-data-categories.js
@@ -16,6 +16,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("events", null, {});
+    await queryInterface.bulkDelete("categories", null, {});
   },
 };
