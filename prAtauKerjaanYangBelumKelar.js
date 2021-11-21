581a4faaa060e8082dbce644f8657fe502751a69
Mochamad Dena Eka Putra <sica4822@gmail.com>
2021-11-21 14:22:41 +0700

    remove pr

--- prAtauKerjaanYangBelumKelar.js
+++
@@ -1,9 +0,0 @@
-// di comment masalah nampil, di order by created at
-
-// Pagination di myEvent dan getAllBookmark belum bisa berjalan
-
-// Tanya mengenai ngambil event id
-
-// Diskusi masalah tanggal event
-
-// Diskusi masalah req.params dan body
