5a08e81dafaa7832ce8343843b9f9af0b53d5628
Agif12 <ahyarsagipul@gmail.com>
2021-11-25 15:35:26 +0800

    Try CI/CD Again

--- config/config.js
+++ config/config.js
@@ -13,19 +13,20 @@ module.exports = {
     timezone: "+07:00", // for writing to database
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: "dena",
+    password: null,
+    database: "seeEventTeamD_test",
+    host: "localhost",
+    dialect: "postgres",
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: "pglranbneidtaj",
+    password:
+      "859d93132a1cdba1b59f803031235313f324e7c448a8e3d5105df2ba42995e4f",
+    database: "d3e64ki6lhtad8",
+    host: "ec2-34-198-189-252.compute-1.amazonaws.com",
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false,
