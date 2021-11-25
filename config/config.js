5ec0e0493c0ab3e441ddf53503731e1b1e712c19
Agif12 <ahyarsagipul@gmail.com>
2021-11-26 01:52:18 +0800

    CI/CD

--- config/config.js
+++ config/config.js
@@ -13,17 +13,12 @@ module.exports = {
     timezone: "+07:00", // for writing to database
   },
   test: {
-    username: "pglranbneidtaj",
-    password:
-      "859d93132a1cdba1b59f803031235313f324e7c448a8e3d5105df2ba42995e4f",
-    database: "d3e64ki6lhtad8",
-    host: "ec2-34-198-189-252.compute-1.amazonaws.com",
+    username: "dena",
+    password: null,
+    database: "seeEventTeamD_test",
+    host: "localhost",
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    logging: false,
   },
   production: {
     username: "pglranbneidtaj",
